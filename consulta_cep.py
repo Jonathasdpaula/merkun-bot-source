da31068bb58d89e01761b5b8da9d26f910be0bed
Luca'zz Dev <luc4zzd3v@gmail.com>
2021-02-14 14:32:48 -0300

    Update consulta_cep.py

--- consulta_cep.py
+++ consulta_cep.py
@@ -4,7 +4,7 @@ def cep(message):
 
     try:
         request = requests.get('https://brasilapi.com.br/api/cep/v1/' + message.text.replace('/cep ', '')).json()
-        cep = message.text.replace('/cep ', '')
+        cep = request.get('cep')
         cidade = request.get('city')
         uf = request.get('state')
         vizinhanca = request.get('neighborhood')
