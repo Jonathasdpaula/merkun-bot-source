f04c609b7253241e041f8582ab0cee0999eea24e
Luca'zz Dev <luc4zzd3v@gmail.com>
2021-02-17 15:39:30 -0300

    Update consulta_ip.py

--- consulta_ip.py
+++ consulta_ip.py
@@ -5,11 +5,6 @@ def ip(message):
      request = requests.get('http://ip-api.com/json/' + message.text.replace('https://', '').replace('http://', '').replace('/ip ', '')).json()
 
      if 'message' not in request:
-        cep = request.get('cep')
-        cidade = request.get('city')
-        uf = request.get('state')
-        vizinhanca = request.get('neighborhood')
-        rua = request.get('street')
 
         return f'''
 IP: {request.get('query')}
