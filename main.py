5b87c44808e9a56ff5dcc0703b5605b350f4fc29
Luca'zz Dev <luc4zzd3v@gmail.com>
2021-02-18 20:40:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import telebot                     
+import telebot
 from start_menu import menu
 from consultar_ip import ip
 from consultar_bin import bin
@@ -30,31 +30,35 @@ def consulta_bin(message):
     bot.reply_to(message, bin(message))
 
 
-
 @bot.message_handler(commands=['placa'])
 def consulta_placa(message):
 
     bot.reply_to(message, placa(message))
 
+
 @bot.message_handler(commands=['cep'])
-def consulta_placa(message):
+def consulta_cep(message):
 
     bot.reply_to(message, cep(message))
+
 
 @bot.message_handler(commands=['gerarcpf'])
 def gera_cpf(message):
 
     bot.reply_to(message, gen_cpf(message))
 
+
 @bot.message_handler(commands=['gerarcnpj'])
-def gera_cpf(message):
+def gera_cnpj(message):
 
     bot.reply_to(message, gen_cnpj(message))
 
+
 @bot.message_handler(commands=['covid'])
-def gera_cp(message):
+def covidstats(message):
 
     bot.reply_to(message, covid(message))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     bot.polling(none_stop=True)
