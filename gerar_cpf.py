1a766ecfab04042c10c609bf3a16a52ca381ad38
Luca'zz Dev <luc4zzd3v@gmail.com>
2021-02-19 21:41:17 -0300

    Update gerar_cpf.py

--- gerar_cpf.py
+++ gerar_cpf.py
@@ -4,7 +4,7 @@ import requests
 def gen_cpf(message):
 
     request = requests.get('http://geradorapp.com/api/v1/cpf/generate?token=SEU TOKEN AQUI' +
-                           message.text.replace('/gerarcpf', '')).json()
+                           message.text.replace('@merkun_bot', '').replace('/gerarcpf', '')).json()
     dados = request.get('data')
     numero = dados.get('number_formatted')
 
