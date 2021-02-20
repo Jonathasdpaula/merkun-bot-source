3fbfe40c3e4db7f6f5095ea0c609977731d38884
Luca'zz Dev <luc4zzd3v@gmail.com>
2021-02-19 21:41:39 -0300

    Update gerar_cnpj.py

--- gerar_cnpj.py
+++ gerar_cnpj.py
@@ -4,7 +4,7 @@ import requests
 def gen_cnpj(message):
 
     request = requests.get('http://geradorapp.com/api/v1/cnpj/generate?token=SEU TOKEN AQUI' +
-                           message.text.replace('/gerarcnpj', '')).json()
+                           message.text.replace('@merkun_bot', '').replace('/gerarcnpj', '')).json()
     dados = request.get('data')
     numero = dados.get('number_formatted')
 
